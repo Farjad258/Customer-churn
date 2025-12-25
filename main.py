57d72873e4294020d820a137a5e69fc0438ab2be
FARJAD DANISH <farjadkhan258@gmail.com>
2025-12-25 17:30:16 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pandas as pd
+import joblib
+import os
+
+app = FastAPI(title="Customer Churn Prediction API")
+
+# Load model and feature list
+BASE_DIR = os.path.dirname(os.path.abspath(_file_))
+model = joblib.load(os.path.join(BASE_DIR, "churn_model.pkl"))
+features = joblib.load(os.path.join(BASE_DIR, "model_features.pkl"))
+
+# Input schema
+class CustomerFeatures(BaseModel):
+    gender: int
+    SeniorCitizen: int
+    Partner: int
+    Dependents: int
+    tenure: float
+    MonthlyCharges: float
+    TotalCharges: float
+
+@app.get("/")
+def home():
+    return {"message": "Customer Churn Prediction API is running"}
+
+@app.post("/predict")
+def predict(data: CustomerFeatures):
+    input_dict = data.dict()
+
+    # Create dataframe with correct columns
+    input_df = pd.DataFrame([input_dict])
+    input_df = input_df.reindex(columns=features, fill_value=0)
+
+    prediction = model.predict(input_df)[0]
+    probability = model.predict_proba(input_df)[0][1]
+
+    return {
+        "churn_prediction": int(prediction),
+        "churn_probability": round(float(probability), 3)
+    }
